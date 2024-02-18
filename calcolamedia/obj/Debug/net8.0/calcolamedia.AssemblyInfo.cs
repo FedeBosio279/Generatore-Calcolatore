74e79ce627c5f3d887ec18551297aa855496bce3
Federico Bosio <federicobosio0209@gmail.com>
2024-02-18 18:20:20 +0100

    .

--- calcolamedia/obj/Debug/net8.0/calcolamedia.AssemblyInfo.cs
+++ calcolamedia/obj/Debug/net8.0/calcolamedia.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("calcolamedia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e83d3fe8fa02f4a2b554da800c2f39c524965fc9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+698d1831ae166db6586d00a9066087ccb50580b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("calcolamedia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("calcolamedia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
